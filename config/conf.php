fd13d0d47817a76643b9ed735b6266d4765f51a1
Anser Omar <anseromar@outlook.fr>
2019-03-25 19:27:47 +0100

    Adding my final project to framework-MVC-php repository.

---
+++ config/conf.php
@@ -0,0 +1,27 @@
+<?php
+class Conf{
+	
+	static $debug = 1; 
+
+	static $databases = array(
+
+		'default' => array(
+			'host'		=> 'localhost',
+			'database'	=> 'tuto',
+			'login'		=> 'root',
+			'password'	=> ''
+		)
+	);
+
+
+}
+
+Router::prefix('cockpit','admin');
+
+
+Router::connect('','posts/index');
+Router::connect('cockpit','cockpit/posts/index');
+Router::connect('page/:slug-:id','pages/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
+Router::connect('blog/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
+Router::connect('blog/category/:slug','posts/category/slug:([a-z0-9\-]+)');
+Router::connect('blog/*','posts/*');
